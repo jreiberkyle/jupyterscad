4498b1103a5094add36ea2e6f0c50cd9cea23f2f
jreiberkyle <jreiberkyle@users.noreply.github.com>
2024-02-26 12:02:12 -0800

    black formatting

--- tests/test_render.py
+++ tests/test_render.py
@@ -14,6 +14,7 @@ PARTICULAR PURPOSE. See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with
 this program. If not, see <https://www.gnu.org/licenses/>.
 """
+
 import logging
 import shutil
 import subprocess
